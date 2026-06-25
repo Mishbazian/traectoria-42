5b28057568cd5601f914cdd4c55357ef66cb1fcd
Mikhail Ivanov <shirokopuh@gmail.com>
2026-06-25 18:29:35 +0700

    init frontend

---
+++ frontend/prettier.config.js
@@ -0,0 +1,13 @@
+const config = {
+	semi: true,
+	trailingComma: 'es5',
+	singleQuote: true,
+	jsxSingleQuote: true,
+	bracketSameLine: true,
+	printWidth: 80,
+	useTabs: true,
+	tabWidth: 2,
+	endOfLine: 'lf',
+};
+
+export default config;
