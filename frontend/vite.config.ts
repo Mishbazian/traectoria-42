994647a5df80e96b19ad1613ef273ecc9360f8af
Mikhail Ivanov <shirokopuh@gmail.com>
2026-06-27 01:49:50 +0700

    chore(frontend): Add and configure Jotai DevTools

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,10 +2,17 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
+import babel from '@rolldown/plugin-babel';
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), tailwindcss()],
+	plugins: [
+		react(),
+		tailwindcss(),
+		babel({
+			presets: ['jotai-babel/preset'],
+		}),
+	],
 	resolve: {
 		alias: {
 			'@': path.resolve(__dirname, './src'),
