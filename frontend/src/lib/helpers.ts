23ee36d0ac52762273043f67908dc0c7df6265a0
Mikhail Ivanov <shirokopuh@gmail.com>
2026-07-08 23:27:18 +0700

    feat(helpers): add utils to save and load LS

---
+++ frontend/src/lib/helpers.ts
@@ -0,0 +1,14 @@
+// Записывает данные в LocalStorage
+export function setLocalStorage(key: string, value: unknown): void {
+	try {
+		localStorage.setItem(key, JSON.stringify(value));
+	} catch (error) {
+		console.error('Ошибка при сохранении в localStorage:', error);
+	}
+}
+
+// Получает значение из локального хранилища
+export function getFromLocalStorage<T>(key: string): T | null {
+	const data = localStorage.getItem(key);
+	return data ? JSON.parse(data) : null;
+}
