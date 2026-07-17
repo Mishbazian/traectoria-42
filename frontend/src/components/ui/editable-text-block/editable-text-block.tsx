ae0d408c885b95ae2a5f1fc91948be37f520c3df
Mikhail Ivanov <shirokopuh@gmail.com>
2026-07-17 14:35:49 +0700

    refactor(editable-text-block): refactor component and move to components/ui
    - add forwardRef
    - extends API by HTML element props
    - fix hover effect by group/edit classname

---
+++ frontend/src/components/ui/editable-text-block/editable-text-block.tsx
@@ -0,0 +1,76 @@
+import { useState, forwardRef } from 'react';
+import { Button } from '../button';
+import { Pen } from 'lucide-react';
+
+import type { EditableTextBlockProps } from './types';
+import { InlineEditor } from '../../inline-editor';
+
+export const EditableTextBlock = forwardRef<
+	HTMLDivElement,
+	EditableTextBlockProps
+>(
+	(
+		{
+			as: Tag = 'h2',
+			item,
+			prepend,
+			append,
+			onSubmit,
+			onCancel,
+			cancelByOutsideClick = false,
+			...props
+		},
+		ref
+	) => {
+		const [isEditing, setIsEditing] = useState(false);
+
+		const handleSave = (newTitle: string) => {
+			onSubmit?.(newTitle);
+			setIsEditing(false);
+		};
+
+		const handleClose = () => {
+			onCancel?.();
+			setIsEditing(false);
+		};
+
+		return (
+			<div className='flex items-center gap-2 p-2 rounded' ref={ref} {...props}>
+				{!isEditing ? (
+					<>
+						{prepend && <>{prepend}</>}
+						<div className='flex group/editor items-center h-full hover:bg-accent transition-colors rounded'>
+							<Tag
+								className='truncate px-2'
+								tabIndex={0}
+								onClick={() => setIsEditing(true)}
+								title='Нажмите, чтобы отредактировать'>
+								{item.title}
+							</Tag>
+							<Button
+								variant='ghost'
+								size='icon'
+								onClick={() => {
+									setIsEditing(true);
+								}}
+								className='h-full opacity-0 group-hover/editor:opacity-100 focus:opacity-100 transition-opacity'
+								title='Редактировать'
+								aria-label='Редактировать заголовок'>
+								<Pen className='text-muted-foreground' />
+							</Button>
+							{append && <>{append}</>}
+						</div>
+					</>
+				) : (
+					<InlineEditor
+						value={item.title}
+						onSubmit={handleSave}
+						onCancel={handleClose}
+						placeholder='Введите заголовок'
+						closeOnOutsideClick={cancelByOutsideClick}
+					/>
+				)}
+			</div>
+		);
+	}
+);
