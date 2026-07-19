20cef791df3888b8b6e40d4958ca21d84bd85003
Mikhail Ivanov <shirokopuh@gmail.com>
2026-07-19 11:41:48 +0700

    fix(grabbing-grip): add missed props

--- frontend/src/components/ui/grabbing-grip/types.ts
+++ frontend/src/components/ui/grabbing-grip/types.ts
@@ -1,5 +1,8 @@
+import type { ClassNameValue } from 'tailwind-merge';
+
 export interface GrabbingGripProps {
 	variant?: 'default' | 'vertical' | 'horizontal';
 	isGrabbing: boolean;
 	size?: number;
+	className?: ClassNameValue;
 }
