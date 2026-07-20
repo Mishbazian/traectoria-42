faf00596d1c3fcf74f2a85f3c122b28e42c75176
Mikhail Ivanov <shirokopuh@gmail.com>
2026-07-21 00:11:08 +0700

    feat(kanban) add gap between boards

--- frontend/src/components/kanban-content/kanban-content.tsx
+++ frontend/src/components/kanban-content/kanban-content.tsx
@@ -40,7 +40,7 @@ export const KanbanContent = observer(() => {
 
 	return (
 		<DragDropProvider {...dragHandlers}>
-			<div className='p-4'>
+			<div className='p-4 flex flex-col gap-2'>
 				{boardsOrder.map((board, boardIndex) => (
 					<SortableBoard
 						key={board.id}
