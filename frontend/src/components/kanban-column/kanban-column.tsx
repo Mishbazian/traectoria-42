e14f0129e3b1e1e852d1f5e7c9ff6703d119a405
Mikhail Ivanov <shirokopuh@gmail.com>
2026-07-22 16:54:45 +0700

    feat(kanban-column): add dropdown menu with edit and delete - added dropdown menu - added local states isEdit, isTryToDdelete - added modal for confirm delete column

--- frontend/src/components/kanban-column/kanban-column.tsx
+++ frontend/src/components/kanban-column/kanban-column.tsx
@@ -5,45 +5,113 @@ import { observer } from 'mobx-react-lite';
 import { EditableTextBlock } from '../ui/editable-text-block';
 import { GrabbingGrip } from '../ui/grabbing-grip';
 import { Button } from '../ui/button';
-import { Plus } from 'lucide-react';
+import { EllipsisVertical, PencilIcon, Plus, TrashIcon } from 'lucide-react';
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuGroup,
+	DropdownMenuItem,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger,
+} from '../ui/dropdown-menu';
+import { useState } from 'react';
+import { DeleteConfirmModal } from '../ui/delete-confirm-modal';
 
 export const KanbanColumn = observer(
 	({ id, ref, handleRef, children, isDragging = false }: KanbanColumnProps) => {
+		const [isEdit, setIsEdit] = useState<boolean>(false);
+		const [isTryToDelete, setIsTryToDelete] = useState<boolean>(false);
+
 		const column = boardStore.columnsMap[id];
+		if (!column) return null;
 
 		const handleHeaderUpdate = async (newTitle: string) => {
 			await boardStore.updateColumn(id, { ...column, title: newTitle });
 		};
-		const handleAddCard = () => {}; //@todo
 
+		const handleDeleteColumn = async () => {
+			try {
+				await boardStore.deleteColumn(id);
+			} finally {
+				setIsTryToDelete(false);
+			}
+		};
+
+		const isDeleteAvailible: boolean = column.cards.length === 0;
+
+		const handleAddCard = () => {}; //@todo
 		return (
-			<section className='border w-sm rounded-xl' ref={ref}>
-				<EditableTextBlock
-					as='h3'
-					className='border relative group/column_header'
-					item={column}
-					append={
-						<GrabbingGrip
-							ref={handleRef}
-							isGrabbing={isDragging}
-							className='w-full flex justify-end'
-						/>
+			<>
+				<section className='border w-sm rounded-xl' ref={ref}>
+					<EditableTextBlock
+						as='h3'
+						className='border relative group/column_header'
+						item={column}
+						prepend={
+							<GrabbingGrip
+								ref={handleRef}
+								isGrabbing={isDragging}
+								size={16}
+								className=''
+							/>
+						}
+						onSave={handleHeaderUpdate}
+						cancelByOutsideClick
+						isEdit={isEdit}
+						append={
+							<DropdownMenu>
+								<DropdownMenuTrigger>
+									<EllipsisVertical
+										size={16}
+										className='text-gray-400 hover:text-gray-700 dark:text-gray-500 '
+									/>
+								</DropdownMenuTrigger>
+								<DropdownMenuContent className='w-fit' align='end'>
+									<DropdownMenuItem onClick={() => setIsEdit(true)}>
+										<PencilIcon />
+										Редактировать
+									</DropdownMenuItem>
+
+									<DropdownMenuSeparator />
+									<DropdownMenuGroup>
+										<DropdownMenuItem
+											variant='destructive'
+											onClick={() => setIsTryToDelete(true)}
+											disabled={!isDeleteAvailible}>
+											<TrashIcon />
+											Удалить
+										</DropdownMenuItem>
+									</DropdownMenuGroup>
+								</DropdownMenuContent>
+							</DropdownMenu>
+						}
+					/>
+					<Button
+						variant='secondary'
+						size='icon-lg'
+						onClick={handleAddCard}
+						className='w-full bg-background/50 hover:bg-primary/10 hover:text-primary'>
+						<Plus />
+					</Button>
+					<ScrollArea className=''>
+						<div className='flex flex-col gap-2 p-2'>{children}</div>
+						<ScrollBar />
+					</ScrollArea>
+				</section>
+				<DeleteConfirmModal
+					title='Удалить колонку?'
+					description={
+						<>
+							Вы действительно хотите удалить колонку{' '}
+							<span className='text-accent-foreground'>{column.title}</span>?
+							<br /> Это действие необратимо!
+						</>
 					}
-					onSave={handleHeaderUpdate}
-					cancelByOutsideClick
+					isOpen={isTryToDelete}
+					onClose={() => setIsTryToDelete(false)}
+					onSubmit={handleDeleteColumn}
 				/>
-				<Button
-					variant='secondary'
-					size='icon-lg'
-					onClick={handleAddCard}
-					className='w-full bg-background/50 hover:bg-primary/10 hover:text-primary'>
-					<Plus />
-				</Button>
-				<ScrollArea className=''>
-					<div className='flex flex-col gap-2 p-2'>{children}</div>
-					<ScrollBar />
-				</ScrollArea>
-			</section>
+			</>
 		);
 	}
 );
