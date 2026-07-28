40313783d1ddf2c4abbe429e3f7e738972fe653c
Mikhail Ivanov <shirokopuh@gmail.com>
2026-07-28 17:18:20 +0700

    feat(matrix-panel): add MatrixCell component

--- frontend/src/widgets/matrix/ui/matrix.tsx
+++ frontend/src/widgets/matrix/ui/matrix.tsx
@@ -5,6 +5,7 @@ import { EditableTextBlock } from '@ui';
 import { getCellId } from '../../../entities/matrix-panel/matrix-board-store';
 import { KanbanCard } from '../../kanban/ui/kanban-card';
 import { cn } from '@lib';
+import { CELL } from '@constants';
 
 // eslint-disable-next-line react-refresh/only-export-components
 const MatrixCell = ({ children }: { children?: ReactNode }) => {
@@ -20,9 +21,9 @@ type MatrixProps = {
 
 export const MatrixPanel: FC<MatrixProps> = observer(({ store }) => {
 	return (
-		<div className='mx-auto my-0 flex w-fit max-w-3/4 flex-wrap gap-2'>
+		<div className='mx-auto my-0 grid w-max max-w-3/4 grid-flow-col grid-cols-[repeat(auto-fill,minmax(fit-content,1fr))] gap-2'>
 			{store.boards.map((board) => (
-				<section className=''>
+				<section className='p-2'>
 					<EditableTextBlock
 						as='h2'
 						title={board.title}
@@ -31,61 +32,60 @@ export const MatrixPanel: FC<MatrixProps> = observer(({ store }) => {
 						className='bg-accent'
 					/>
 					<div
-						className={cn(
-							'relative grid w-max min-w-50 gap-2 p-2',
-							`grid-cols-[repeat(${board.columns.length},200px)]`
-						)}>
-						<div className='col-span-full grid grid-cols-subgrid gap-2'>
-							{board.columns.length > 1 &&
-								board.columns.map((c) => (
-									<EditableTextBlock
-										as='h2'
-										title={c.title}
-										onSave={() => {}}
-										cancelByOutsideClick
-										className=''
-										key={c.id}
-										id={c.id}
-									/>
-								))}
-
-							{board.rows.map((r) => (
-								<>
-									<div
-										className={cn(
-											'col-span-full grid grid-cols-subgrid rounded-lg',
-											board.rows.length > 1 && 'ring'
-										)}>
-										{board.rows.length > 1 && (
-											<EditableTextBlock
-												as='h3'
-												title={r.title}
-												onSave={() => {}}
-												cancelByOutsideClick
-												className='col-span-full mt-3 rounded-none rounded-t-lg'
-											/>
-										)}
-										{board.columns.map((c) => {
-											const cellId = getCellId(board.id, c.id, r.id);
-											const taskList = store.cellsDataMap.get(cellId);
-											return (
-												<MatrixCell key={cellId}>
-													{taskList &&
-														taskList?.map((t) => (
-															<KanbanCard card={t} onCardClick={() => {}} />
-														))}
-												</MatrixCell>
-											);
-										})}
-									</div>
-								</>
+						className='relative col-span-full grid gap-2 p-2 h-full'
+						style={{
+							gridTemplateColumns: `repeat(${board.columns.length},${CELL.width})`,
+						}}>
+						{board.columns.length > 1 &&
+							board.columns.map((c) => (
+								<EditableTextBlock
+									as='h3'
+									title={c.title}
+									onSave={() => {}}
+									cancelByOutsideClick
+									className=''
+									key={c.id}
+									id={c.id}
+								/>
 							))}
-						</div>
+
+						{board.rows.map((r) => (
+							<>
+								<div
+									className={cn(
+										'col-span-full grid grid-cols-subgrid rounded-lg',
+										board.rows.length > 1 && 'ring'
+									)}>
+									{board.rows.length > 1 && (
+										<EditableTextBlock
+											as='h3'
+											title={r.title}
+											onSave={() => {}}
+											cancelByOutsideClick
+											className='col-span-full mt-3 rounded-none rounded-t-lg'
+										/>
+									)}
+									{board.columns.map((c) => {
+										const cellId = getCellId(board.id, c.id, r.id);
+										const taskList = store.cellsDataMap.get(cellId);
+										return (
+											<MatrixCell key={cellId}>
+												{taskList &&
+													taskList?.map((t) => (
+														<KanbanCard card={t} onCardClick={() => {}} />
+													))}
+											</MatrixCell>
+										);
+									})}
+								</div>
+							</>
+						))}
+
 						<div
-							className={cn(
-								'bg-accent absolute inset-0 -z-20 grid gap-2 p-2',
-								`grid-cols-${board.columns.length}`
-							)}>
+							className={cn('bg-accent absolute inset-0 -z-20 grid gap-2 p-2')}
+							style={{
+								gridTemplateColumns: `repeat(${board.columns.length},${CELL.width})`,
+							}}>
 							{board.columns.length > 1 &&
 								board.columns.map((c) => (
 									<div
