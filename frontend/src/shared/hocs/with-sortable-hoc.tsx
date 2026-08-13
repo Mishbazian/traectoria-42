569b8a91f1f771ef7ec078165feb3424101ee5bf
Mikhail Ivanov <shirokopuh@gmail.com>
2026-08-13 16:21:57 +0700

    feat(withSortable): update hoc to accept sortaple config in signature

--- frontend/src/shared/hocs/with-sortable-hoc.tsx
+++ frontend/src/shared/hocs/with-sortable-hoc.tsx
@@ -1,12 +1,16 @@
 import { useSortable, type UseSortableInput } from '@dnd-kit/react/sortable';
 import type { ComponentType, ReactElement } from 'react';
+
+type SortableInputProps = Partial<UseSortableInput>;
 export function withSortable<T>(
-	WrappedComponent: ComponentType<T>
+	WrappedComponent: ComponentType<T>,
+	config?: SortableInputProps
 ): ComponentType<T & UseSortableInput> {
 	return function (props: T & UseSortableInput): ReactElement {
 		const sortableProps = useSortable({
-			...props,
 			id: props.id,
+			index: props.index,
+			...config,
 		});
 
 		return <WrappedComponent {...props} {...sortableProps} />;
