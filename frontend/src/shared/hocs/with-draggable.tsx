1fe1fcb4d1f20530bab717550dfd652a80ff8818
Mikhail Ivanov <shirokopuh@gmail.com>
2026-08-13 20:37:00 +0700

    feat(with-draggable): add hoc for draggable components

---
+++ frontend/src/shared/hocs/with-draggable.tsx
@@ -0,0 +1,20 @@
+import type { ComponentType, ReactElement } from 'react';
+import type { DraggablePreset } from '../lib';
+import {
+	useDraggable,
+	type UseDraggableInput,
+} from '@dnd-kit/react';
+
+export function withDroppable<T>(
+	WrappedComponent: ComponentType<T>,
+	config?: DraggablePreset
+): ComponentType<T & UseDraggableInput> {
+	return function (props: T & UseDraggableInput): ReactElement {
+		const draggableProps = useDraggable({
+			id: props.id,
+			...config,
+		});
+
+		return <WrappedComponent {...props} {...draggableProps} />;
+	};
+}
